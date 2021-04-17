7919d623cf770ad0f71302c5f678dc95f0467e2b
Vadim Kireev <vadim.kireev.89@gmail.com>
2021-04-17 19:08:50 +0500

    trycatch

--- NineLesson/Program.cs
+++ NineLesson/Program.cs
@@ -29,10 +29,11 @@ namespace NineLesson
 
         private static void MessageListener(object sender, Telegram.Bot.Args.MessageEventArgs e)
         {
+            DirectoryInfo directoryInfo = new DirectoryInfo(@"V:\BotFiles");  // Получаем информацию о текущем каталоге
             string text = $"{DateTime.Now.ToLongTimeString()}: {e.Message.Chat.FirstName} {e.Message.Chat.Id} {e.Message.Text}";
-
+            string g = "gimme+";
+            string s = "send+";
             Console.WriteLine($"{text} TypeMessage: {e.Message.Type.ToString()}");
-
 
             if (e.Message.Type == Telegram.Bot.Types.Enums.MessageType.Document)
             {
@@ -41,80 +42,129 @@ namespace NineLesson
                 Console.WriteLine(e.Message.Document.FileSize);
 
                 DownLoad(e.Message.Document.FileId, e.Message.Document.FileName);
-
-
             }
-
-
-            if (e.Message.Text == "123")
+            
+            if(e.Message.Text!=null)
             {
-                FileStream fs = File.OpenRead($"{e.Message.Text}.txt");
-
-
-                InputOnlineFile fl = new InputOnlineFile(fs,e.Message.Text);
-
-                bot.SendDocumentAsync(e.Message.Chat.Id, fl, e.Message.Text);
-
-            }
-
-            if (e.Message.Text == "sendnudes")
-            {
-                string trim = "";
-
-                DirectoryInfo directoryInfo = new DirectoryInfo("");  // Получаем информацию о текущем каталоге
-
-                foreach (var item in directoryInfo.GetFiles())          // Перебираем все файлы текущего каталога
+                if (e.Message.Text.Contains(g))
                 {
-                    bot.SendTextMessageAsync(e.Message.Chat.Id, $"{trim}{item.Name}"); // Выводим информацию о них
+                    GimmeMoney(e);
+                }
+
+                if (e.Message.Text.Contains(s))
+                {
+                    Nudes(e);
+                }
+
+                if (e.Message.Text == "start")
+                {
+                    StartMessage(e);
+                }
+
+                if (e.Message.Text == "sendnudes")
+                {
+                    SendNudes(e, directoryInfo);
                 }
 
             }
-
-
-
-            if (e.Message.Text != null)
-            {
-                e.Message.Text=e.Message.Text.ToUpper();
-                Encoding.RegisterProvider(CodePagesEncodingProvider.Instance);
-                XDocument xml = XDocument.Load("http://www.cbr.ru/scripts/XML_daily.asp");
-                if (e.Message.Text=="USD")
-                {
-                    var messageText = (xml.Elements("ValCurs")
-                    .Elements("Valute")
-                    .FirstOrDefault(x => x.Element("CharCode")
-                    .Value == $"{e.Message.Text}")
-                    .Elements("Value")
-                    .FirstOrDefault()
-                    .Value) ?? "nope";
-
-
-                    bot.SendTextMessageAsync(e.Message.Chat.Id,
-                                            $"{messageText}"
-                                            );
-                }
-                else
-                {
-                    bot.SendTextMessageAsync(e.Message.Chat.Id,
-                        $"{e.Message.Text}"
-                        );
-                }
-
-
-            }
-
 
 
         }
 
+        /// <summary>
+        /// Сохраняем посланый файл в общую папку
+        /// </summary>
+        /// <param name="fileId"></param>
+        /// <param name="path"></param>
         static async void DownLoad(string fileId, string path)
         {
             var file = await bot.GetFileAsync(fileId);
-            FileStream fs = new FileStream("_" + path, FileMode.Create);
+            FileStream fs = new FileStream(@$"V:\BotFiles\{path}", FileMode.Create);
             await bot.DownloadFileAsync(file.FilePath, fs);
             fs.Close();
 
             fs.Dispose();
             
+        }
+
+        /// <summary>
+        /// Выдаем тип команд
+        /// </summary>
+        /// <param name="e"></param>
+        static void StartMessage(Telegram.Bot.Args.MessageEventArgs e)
+        {
+            bot.SendTextMessageAsync(e.Message.Chat.Id,
+                        "\n'gimme+USD'  курс на текущий день." +
+                        "\n'sendnudes' перечень файлов для скачивания." +
+                        "\n'send+filename' файл скачать." +
+                        "\nХочешь посылай файлы сохраним."
+                        );
+        }
+
+        /// <summary>
+        /// Выдаем курс валют 
+        /// </summary>
+        /// <param name="e"></param>
+        static void GimmeMoney(Telegram.Bot.Args.MessageEventArgs e)
+        {
+            int position = e.Message.Text.IndexOf("+");
+            e.Message.Text = e.Message.Text.Substring(position+1);
+            e.Message.Text = e.Message.Text.ToUpper();
+                Encoding.RegisterProvider(CodePagesEncodingProvider.Instance);
+                XDocument xml = XDocument.Load("http://www.cbr.ru/scripts/XML_daily.asp");
+
+            try
+            {           
+
+                var messageText = xml.Elements("ValCurs").
+                    Elements("Valute").
+                    FirstOrDefault(x => x.Element("CharCode").
+                    Value == $"{e.Message.Text}").
+                    Elements("Value").
+                    FirstOrDefault().Value;
+                bot.SendTextMessageAsync(e.Message.Chat.Id,$"{messageText}");
+            }
+            catch 
+            {
+                bot.SendTextMessageAsync(e.Message.Chat.Id, $"Нет такой валюты");
+            }
+
+
+        }
+
+        /// <summary>
+        /// Посылаем список файлов из папки
+        /// </summary>
+        /// <param name="e"></param>
+        static void SendNudes(Telegram.Bot.Args.MessageEventArgs e, DirectoryInfo directoryInfo)
+        {
+
+            foreach (var item in directoryInfo.GetFiles())          // Перебираем все файлы текущего каталога
+            {
+                bot.SendTextMessageAsync(e.Message.Chat.Id, $"{item.Name}"); // Выводим информацию о них
+            }
+        }
+
+        /// <summary>
+        /// Посылаем файл из папки
+        /// </summary>
+        /// <param name="e"></param>
+        static void Nudes(Telegram.Bot.Args.MessageEventArgs e)
+        {
+            int position = e.Message.Text.IndexOf("+");
+            e.Message.Text = e.Message.Text.Substring(position+1);
+            try
+            {
+                FileStream fs = File.OpenRead($@"V:\BotFiles\{e.Message.Text}");
+                 
+                InputOnlineFile fl = new InputOnlineFile(fs, e.Message.Text);
+                bot.SendDocumentAsync(e.Message.Chat.Id, fl, e.Message.Text);
+            }
+            catch
+            {
+                bot.SendTextMessageAsync(e.Message.Chat.Id, $"Нет такого файла");
+            }
+
         }
 
     }
